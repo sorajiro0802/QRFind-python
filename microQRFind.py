c03629df456442e3cdac0abe218feb9e7a36c1a8
sorajiro0802 <sora_potato_0802@icloud.com>
2023-04-27 12:54:45 +0900

    fix readability

--- microQRFind.py
+++ microQRFind.py
@@ -7,7 +7,7 @@ def main():
     camera_id = 0
     window_name = "Micro QR Finder"
     font = cv2.FONT_HERSHEY_SIMPLEX
-    color = (255, 0, 0) # BGR -> Blue
+    color = (255, 0, 0)  # BGR -> Blue
     cap = cv2.VideoCapture(camera_id)
     # prepair microQR detector
     detector = pb.FactoryFiducial(np.uint8).microqr()
@@ -19,13 +19,15 @@ def main():
             # convert image class to match pyboof
             boof_img = pb.ndarray_to_boof(image_mono)
             detector.detect(boof_img)
-            
+
             for mqr in detector.detections:
                 info = mqr.message
                 points = mqr.bounds.convert_tuple()
-                points = [(int(i), int(j)) for i, j in points] # LeftUp, RightUp, RightDown, LeftDown <- int
+                # LeftUp, RightUp, RightDown, LeftDown <- int
+                points = [(int(i), int(j)) for i, j in points]
                 # line enclose mQRCode
-                image = cv2.polylines(image, [np.array(points)], True, color, 5)
+                image = cv2.polylines(
+                    image, [np.array(points)], True, color, 5)
                 # show decoded infomation onto its microQR
                 image = cv2.putText(img=image,
                                     text=info,
@@ -35,15 +37,15 @@ def main():
                                     color=color,
                                     thickness=2,
                                     lineType=cv2.LINE_AA)
-        
+
             cv2.imshow(window_name, image)
-        
+
         # when Esp pressed
-        if cv2.waitKey(5)  & 0xFF==27:
+        if cv2.waitKey(5) & 0xFF == 27:
             break
-    
+
     cv2.destroyWindow(window_name)
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     main()
