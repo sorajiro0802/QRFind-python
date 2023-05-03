e009312363bdd3a158ec4f1ff453e82a1e881247
sorajiro0802 <sora_potato_0802@icloud.com>
2023-05-03 21:58:53 +0900

    add func: convert to CSV with special format

--- qrPicup.py
+++ qrPicup.py
@@ -23,7 +23,9 @@ def main():
     img_path = "./data/mQRArray_rotated.png" #* tmp img
     # select QR or microQR
     decorder = pb.FactoryFiducial(np.uint8).microqr()
-    res = decodeQR(decorder, img_path)
+    img_name = os.path.basename(img_path)
+    num, res = decodeQR(decorder, img_path)
+    convertInfo2CSV(img_name, num, res, saveDirdPath, csv_name)
     
     
 def getFolderAbsPath(cmd_arg):
@@ -41,6 +43,7 @@ def getFolderAbsPath(cmd_arg):
 def decodeQR(decorder, img_path) -> dict:
     image = pb.load_single_band(img_path, np.uint8)
     decorder.detect(image)
+    num = len(decorder.detections)
     res = {}
     for code in decorder.detections:
         info = code.message
@@ -48,12 +51,24 @@ def decodeQR(decorder, img_path) -> dict:
         # convert float num of points to int
         points = [(int(i), int(j)) for i, j in points]
         res[info] = points
-    
     # return dict ex.( {info : [ponts]} )
-    return res
+    return num, res
     
-def convertDict2CSV(res, save_path):
-    pass
+def convertInfo2CSV(img_name, num, dict_, save_dir, save_name):
+    # format : <imgFileName>, <number of detected QRCode>, (x0,y0),(x1,y1),(x2,y2),(x3,y3), <decode string>, ...
+    save_path = f"{save_dir}/{save_name}"
+    with open(save_path, "w") as f:
+        writer = csv.writer(f)
+        writeLine = []
+        writeLine.append(img_name)  # imgFileName
+        writeLine.append(num)       # number of detected QRCodes
+        # prepare csv form of QR infomations
+        for info, points in dict_.items():
+            writeLine.append(info)
+            for x, y in points:
+                writeLine.append(x)
+                writeLine.append(y)
+        writer.writerow(writeLine)
 
 
 if __name__=="__main__":
